9e3f5087e4f9154c8803803f4fc66e14adfa8db7
Ziyaad <rhyman.ziyaad@gmail.com>
2021-09-04 11:24:17 +0400

    [+][ADD] Add height and width to text

---
+++ app/src/main/java/com/ziyaad/simpletextcomposeapplication/Composables.kt
@@ -0,0 +1,6 @@
+package com.ziyaad.simpletextcomposeapplication
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+/* Import all re-usable composables here */
--- app/src/main/java/com/ziyaad/simpletextcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/ziyaad/simpletextcomposeapplication/MainActivity.kt
@@ -3,36 +3,55 @@ package com.ziyaad.simpletextcomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ziyaad.simpletextcomposeapplication.ui.theme.SimpleTextComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SimpleTextComposeApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    GreetingText(name = "world")
                 }
             }
-        }
-    }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun GreetingText(name: String) {
+    Text(text = "Hello $name!",
+    modifier = Modifier
+            /* Set height seperately
+            .height(240.dp)
+            .width(80.dp) */
+
+            /* Set height and width */
+            .size(width = 240.dp, height = 80.dp)
+
+            /* Set height or width full screen
+            .fillMaxSize() */
+
+            /* Set height or width fraction of screen
+            .fillMaxWidth(0.5f) */
+    )
+}
+
+@Composable
+fun GreetingButton(){
+    Button(onClick = {  }) {
+        GreetingText(name = "button")
+        GreetingText(name = "remaining button")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    SimpleTextComposeApplicationTheme {
-        Greeting("Android")
-    }
+        GreetingText(name = "world")
 }
